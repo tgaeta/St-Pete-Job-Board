9da7217d9964e80e8fad77b762f91f1ab2c5650b
Tony Gaeta <tony.gaeta@gmail.com>
2015-03-30 18:20:14 -0400

    created views for company signup

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ActiveRecord::Base
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true,
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
 end
