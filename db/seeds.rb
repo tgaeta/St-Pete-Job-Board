d55bc883246ecb473e7bfd58e66ca945cd2ed54a
Tony Gaeta <tony.gaeta@gmail.com>
2015-04-05 16:28:24 -0400

    added remote_logo to company for viewing url logos, added pagination, seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+JobApplication.destroy_all
+JobPost.destroy_all
+Company.destroy_all
+
+puts 'Creating companies and job posts...'
+
+10.times do |x|
+  puts "Creating company #{x}"
+  logos = %w(Birchwood.png, jannus.jpg, kahwa.jpg, madmobile.png)
+  Company.create!(remote_logo_url: Faker::Company.logo,
+                  name: Faker::Company.name + Faker::Company.suffix,
+                  email: Faker::Internet.email,
+                  url: Faker::Internet.url,
+                  bio: Faker::Lorem.paragraph(2),
+                  password: 'password',
+                  password_confirmation: 'password',
+                  admin: false)
+end
+
+Company.create!(logo: 'jannus.jpg',
+                name: Faker::Company.name + Faker::Company.suffix,
+                email: 'test@gmail.com',
+                url: Faker::Internet.url,
+                bio: Faker::Lorem.paragraph(2),
+                password: 'password',
+                password_confirmation: 'password',
+                admin: true)
+
+puts 'Creating job post for each company...'
+
+Company.all.each do |company|
+  11.times do |x|
+    tags = ['admin/office', 'business', 'customer service', 'education', 'engineering', 'finance', 'food/bev/hosp', 'general labor', 'government', 'healthcare', 'human resources', 'internet', 'engineering', 'legal', 'manufacturing', 'marketing', 'media', 'nonprofit', 'real estate', 'retail/wholesale', 'sales, salon/spa/fitness', 'science', 'security', 'skilled trades', 'software', 'systems/networking', 'tech support', 'transport', 'tv/video/radio', 'web design/development', 'writing']
+    uid = Company.ids
+    puts "Creating job post #{x} for company #{company.name}"
+    JobPost.create!(company_id: uid.shuffle.sample,
+                    job_title: Faker::Name.title,
+                    job_description: Faker::Lorem.paragraph(4) + Faker::Lorem.paragraph(3),
+                    job_tag: tags.shuffle.sample)
+  end
+end
