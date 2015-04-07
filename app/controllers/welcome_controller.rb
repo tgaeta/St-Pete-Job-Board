e46b7b159ec385abd8b8ca7675f176a2027d37db
Tony Gaeta <tony.gaeta@gmail.com>
2015-04-07 15:31:45 -0400

    fixed search by downcasing user input and stripping leading and trailing whitespace

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @companies = Company.all
-    @jobs = JobPost.where('lower(job_title) || lower(job_description) ||
-                           lower(job_tag) || upper(job_title) ||
-                           upper(job_description) || upper(job_tag) LIKE ?',
-                          "%#{params[:search]}%").order('created_at DESC')
-            .page(params[:page]).per(10)
+    if params[:search].present?
+      @jobs = JobPost.where('lower(job_title) || lower(job_description) ||
+                             lower(job_tag) LIKE ?',
+                            "%#{params[:search].downcase.strip}%").order('created_at DESC')
+              .page(params[:page]).per(10)
+    else
+      @jobs = JobPost.all.page(params[:page]).per(10)
+    end
   end
 end
