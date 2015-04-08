871622e7ef0fed90c3c653b51e99a6093e192c48
Tony Gaeta <tony.gaeta@gmail.com>
2015-04-08 15:13:19 -0400

    removed unnecessary js, fixed sitemap, changed search blog to search

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,10 +1,6 @@
 class SitemapController < ApplicationController
   respond_to :xml
   def index
-    @jobs = JobPost.where('lower(job_title) || lower(job_description) ||
-                           lower(job_tag) || upper(job_title) ||
-                           upper(job_description) || upper(job_tag) LIKE ?',
-                          "%#{params[:search]}%").order('created_at')
-            .page(params[:page]).per(10)
+    @jobs = JobPost.all
   end
 end
